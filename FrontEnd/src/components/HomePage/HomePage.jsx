c65ec79d69e434a7371ec825ac57f8aeee47a985
KhoaAnhDoNguyen <dokhoahay1@gmail.com>
2024-04-12 17:21:08 +0700

    Add film showing screen

--- FrontEnd/src/components/HomePage/HomePage.jsx
+++ FrontEnd/src/components/HomePage/HomePage.jsx
@@ -6,10 +6,35 @@ import Header from '../SharePages/Header/Header.jsx';
 function HomePage() {
 
   return (
-    <div>
+    <div className='homepage-container'>
       <Header />
-      
-        <div style={{color:'black', marginTop:'500px'}}>Hello</div>
+      <div className='homepage-main'>
+        <div className='movie-showing'>
+            <div className='ms-text'>PHIM ĐANG CHIẾU</div>
+            <div className='ms-film'>
+                <div className='ms-film-detail'>
+                    <img src=".\src\assets\quy-thuat.png" alt="FilmLogo" className="ms-film-img" />
+                    <div className='ms-film-title'>QUỶ THUẬT</div>
+                </div>
+
+                <div className='ms-film-detail'>
+                    <img src=".\src\assets\quy-thuat.png" alt="FilmLogo" className="ms-film-img" />
+                    <div className='ms-film-title'>QUỶ THUẬT</div>
+                </div>
+
+                <div className='ms-film-detail'>
+                    <img src=".\src\assets\quy-thuat.png" alt="FilmLogo" className="ms-film-img" />
+                    <div className='ms-film-title'>QUỶ THUẬT</div>
+                </div>
+
+                <div className='ms-film-detail'>
+                    <img src=".\src\assets\quy-thuat.png" alt="FilmLogo" className="ms-film-img" />
+                    <div className='ms-film-title'>QUỶ THUẬT</div>
+                </div>
+            </div>
+        </div>
+    
+      </div>
     </div>
   )
 }
