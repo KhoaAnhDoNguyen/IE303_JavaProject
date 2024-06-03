93e5a607b4f7ee8fdcff4a6ac4ee2bb4046c2fc7
KhoaAnhDoNguyen <dokhoahay1@gmail.com>
2024-06-03 14:29:14 +0700

    Finish BotChat

--- FrontEnd/src/components/ChatBot/ChatBot.jsx
+++ FrontEnd/src/components/ChatBot/ChatBot.jsx
@@ -1,6 +1,7 @@
-// ChatBot.jsx
 import React, { useState } from 'react';
+import axios from 'axios';
 import './ChatBot.css';
+
 
 const ChatBot = ({ onClose }) => {
   const [message, setMessage] = useState('');
@@ -11,16 +12,10 @@ const ChatBot = ({ onClose }) => {
       const newMessage = { role: 'user', content: message };
       setChatHistory(prevHistory => [...prevHistory, newMessage]); // Thêm tin nhắn mới vào cuối mảng
       setMessage('');
-  
-      const response = await fetch('http://127.0.0.1:5000/chat', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ messages: [...chatHistory, newMessage] })
-      });
-      const data = await response.json();
-      const botResponse = { role: 'bot', content: data.response };
+
+      const response = await axios.post('http://127.0.0.1:5000/chat', { message: message });
+      const botResponse = { role: 'bot', content: response.data.response };
+      console.log(response)
       setChatHistory(prevHistory => [...prevHistory, botResponse]); // Thêm câu trả lời từ bot vào cuối mảng
     } catch (error) {
       console.error('Error:', error);
@@ -43,16 +38,19 @@ const ChatBot = ({ onClose }) => {
         ))}
       </div>
       <div className="input-container">
-        <input 
-          type="text" 
-          value={message} 
-          onChange={(e) => setMessage(e.target.value)} 
+        <input
+          type="text"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
           onKeyPress={handleKeyPress} // Thêm sự kiện khi nhấn phím
-          placeholder="Nhập câu hỏi của bạn tại đây" 
+          placeholder="Nhập câu hỏi của bạn tại đây"
         />
         <button onClick={sendMessage}>Gửi</button>
       </div>
-      <button className="close-button" onClick={onClose}>X</button>
+      <div className="close-container">
+        <div className="customer-care">Chăm sóc khách hàng - K Bot</div>
+        <button className="close-button" onClick={onClose}>X</button>
+      </div>
     </div>
   );
 };
