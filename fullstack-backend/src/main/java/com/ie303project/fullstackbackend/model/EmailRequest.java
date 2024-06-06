8624b6cceebfd75babd6de700d6870d58871b519
KhoaAnhDoNguyen <dokhoahay1@gmail.com>
2024-06-06 15:40:32 +0700

    Design mail

--- fullstack-backend/src/main/java/com/ie303project/fullstackbackend/model/EmailRequest.java
+++ fullstack-backend/src/main/java/com/ie303project/fullstackbackend/model/EmailRequest.java
@@ -4,6 +4,17 @@ public class EmailRequest {
     private String email;
     private String seatName;
     private String message;
+    private String filmName;
+    private String namecinema;
+    private String address;
+    private String room;
+    private String timeshow;
+    private String dayshow;
+    private String dateshow;
+    private String yearshow;
+    private String typeticket;
+    private String nameticket;
+    private String price;
 
     // Getters and Setters
 
@@ -29,5 +40,93 @@ public class EmailRequest {
 
     public void setMessage(String message) {
         this.message = message;
+    }
+
+    public String getFilmName() {
+        return filmName;
+    }
+
+    public void setFilmName(String filmName) {
+        this.filmName = filmName;
+    }
+
+    public String getNamecinema() {
+        return namecinema;
+    }
+
+    public void setNamecinema(String namecinema) {
+        this.namecinema = namecinema;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getRoom() {
+        return room;
+    }
+
+    public void setRoom(String room) {
+        this.room = room;
+    }
+
+    public String getTimeshow() {
+        return timeshow;
+    }
+
+    public void setTimeshow(String timeshow) {
+        this.timeshow = timeshow;
+    }
+
+    public String getDayshow() {
+        return dayshow;
+    }
+
+    public void setDayshow(String dayshow) {
+        this.dayshow = dayshow;
+    }
+
+    public String getDateshow() {
+        return dateshow;
+    }
+
+    public void setDateshow(String dateshow) {
+        this.dateshow = dateshow;
+    }
+
+    public String getYearshow() {
+        return yearshow;
+    }
+
+    public void setYearshow(String yearshow) {
+        this.yearshow = yearshow;
+    }
+
+    public String getTypeticket() {
+        return typeticket;
+    }
+
+    public void setTypeticket(String typeticket) {
+        this.typeticket = typeticket;
+    }
+
+    public String getNameticket() {
+        return nameticket;
+    }
+
+    public void setNameticket(String nameticket) {
+        this.nameticket = nameticket;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
     }
 }
--- fullstack-backend/src/main/java/com/ie303project/fullstackbackend/service/EmailService.java
+++ fullstack-backend/src/main/java/com/ie303project/fullstackbackend/service/EmailService.java
@@ -17,7 +17,23 @@ public class EmailService {
             SimpleMailMessage message = new SimpleMailMessage();
             message.setTo(emailRequest.getEmail());
             message.setSubject("Xác nhận đặt vé thành công");
-            message.setText("Cảm ơn bạn đã đặt vé! Tên ghế của bạn là: " + emailRequest.getSeatName() + "\n\n" + emailRequest.getMessage());
+            message.setText("\t\t\t"+"THÔNG TIN CHI TIẾT VÉ!" + "\n\n" +
+                    "Tên ghế của bạn là: " + emailRequest.getSeatName() + "\n" +
+                    "Loại ghế: " +  emailRequest.getNameticket() + "\n\n" +
+                    "\t\t\t\t" + "Thông tin phim" + "\n" +
+                    "Tên phim: " + emailRequest.getFilmName() + "\n" +
+                    "Ngày công chiếu: " + emailRequest.getDayshow() + ", " + emailRequest.getDateshow() + "/" + emailRequest.getYearshow() + "\n\n" +
+                    "\t\t\t\t" + "Thông tin rạp" + "\n" +
+                    "Tên rạp: " + emailRequest.getNamecinema() + "\n" +
+                    "Số phòng: " + emailRequest.getRoom() + "\n" +
+                    "Địa chỉ: " + emailRequest.getAddress() + "\n\n" +
+                    "\t\t\t\t" + "Thông tin vé" + "\n" +
+                    "Loại vé: " + emailRequest.getTypeticket() + "\n" +
+                    "Giá tiền tổng: " + emailRequest.getPrice() + "đ" + " (ĐÃ THANH TOÁN)" +
+                     "\n\n" + emailRequest.getMessage() + "\n" +
+                    "Người làm đơn:  Admin Khoa"
+
+            );
             message.setFrom("your-email@gmail.com");
             mailSender.send(message);
         } catch (Exception e) {
